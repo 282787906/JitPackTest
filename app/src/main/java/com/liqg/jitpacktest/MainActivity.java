febbac1e670c3a23dbe510cc196d8aa05248f326
Liqg <282787906@qq.com>
2017-06-30 11:11:14 +0800

    test

--- app/src/main/java/com/liqg/jitpacktest/MainActivity.java
+++ app/src/main/java/com/liqg/jitpacktest/MainActivity.java
@@ -2,6 +2,9 @@ package com.liqg.jitpacktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.liqg.jitpacklibrary.LiTest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LiTest liTest=new LiTest();
+        Log.d("MainActivity", liTest.getName());
     }
 }
---
+++ jitpacklibrary2/src/main/java/com/liqg/jitpacklibrary2/LiTest2.java
@@ -0,0 +1,11 @@
+package com.liqg.jitpacklibrary2;
+
+/**
+ * Created by Liqg on 2017/6/30.
+ */
+
+public class LiTest2 {
+    public  String getName(){
+        return  "liqg2";
+    }
+}
